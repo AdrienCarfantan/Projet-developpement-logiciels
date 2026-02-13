829578f89911e17be8fe73a147035571d2dc7e20
Adrien Carfantan <adrien.carfantan@etu.univ-tours.fr>
2026-02-13 15:07:04 +0100

    Carfantan-08-ui-menu-minimal

---
+++ main.py
@@ -0,0 +1,15 @@
+from ui import afficher_menu, demander_choix_menu
+
+def main():
+    while True:
+        afficher_menu()
+        choix = demander_choix_menu()
+        
+        if choix == "7":
+            print("Au revoir !")
+            break
+        else:
+            print(f"Option {choix} sélectionnée (pas encore implémentée).")
+
+if __name__ == "__main__":
+    main()
