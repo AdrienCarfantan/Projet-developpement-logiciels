c1c4ab369104abfa7b16e0e37658b349143cb696
Adrien Carfantan <adrien.carfantan@etu.univ-tours.fr>
2026-02-13 14:31:29 +0100

    Carfantan- 04-serialisation-client-vehicule

--- Models/clients.py
+++ Models/clients.py
@@ -10,3 +10,25 @@ class Client:
 
     def __str__(self):
         return f"{self.id_client} - {self.prenom} {self.nom} ({self.mail})"
+
+
+    def to_dict(self):
+        return {
+            "id_client": self.id_client,
+            "nom": self.nom,
+            "prenom": self.prenom,
+            "mail": self.mail,
+            "telephone": self.telephone,
+            "adresse": self.adresse
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(
+            data["id_client"],
+            data["nom"],
+            data["prenom"],
+            data["mail"],
+            data["telephone"],
+            data["adresse"]
+        )
