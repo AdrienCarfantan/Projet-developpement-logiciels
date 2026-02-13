b73381d98878611ba09d65f821d8dd0ac39614d2
Adrien Carfantan <adrien.carfantan@etu.univ-tours.fr>
2026-02-13 15:32:59 +0100

    Carfantan-12-data-manager-reservations

--- data_manager.py
+++ data_manager.py
@@ -43,3 +43,46 @@ if __name__ == "__main__":
     vehicules = charger_vehicules()
     for v in vehicules:
         print(v)
+
+
+import json
+from Models.reservations import Reservation
+
+RESERVATIONS_FILE = "reservations.json"
+
+def charger_reservations(fichier=RESERVATIONS_FILE):
+    try:
+        with open(fichier, "r", encoding="utf-8") as f:
+            data = json.load(f)
+        reservations = [Reservation.from_dict(r) for r in data]
+        print(f"✓ {len(reservations)} réservation(s) chargée(s)")
+        return reservations
+    except FileNotFoundError:
+        print(f" Fichier {fichier} introuvable")
+        return []
+    except json.JSONDecodeError:
+        print(f" JSON invalide dans {fichier}")
+        return []
+
+def sauvegarder_reservation(reservation: Reservation, fichier=RESERVATIONS_FILE):
+    reservations = charger_reservations(fichier)
+    reservations.append(reservation)
+    data = [r.to_dict() for r in reservations]
+
+    with open(fichier, "w", encoding="utf-8") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
+
+    print(f"✓ Réservation sauvegardée dans {fichier}")
+    return True
+
+def generer_id_reservation(fichier=RESERVATIONS_FILE):
+    reservations = charger_reservations(fichier)
+    if not reservations:
+        return "R0001"
+    
+    dernier_id = max(int(r.id_reservation[1:]) for r in reservations)
+    return f"R{dernier_id + 1:04d}"
+
+def filtrer_reservations_par_client(id_client: str, fichier=RESERVATIONS_FILE):
+    reservations = charger_reservations(fichier)
+    return [r for r in reservations if r.id_client == id_client]
