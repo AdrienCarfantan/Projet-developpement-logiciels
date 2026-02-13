40f7ac719adca203c756617a7516233efde1e8a0
Adrien Carfantan <adrien.carfantan@etu.univ-tours.fr>
2026-02-13 14:24:27 +0100

    Carfantan- 03-impl-client-vehicule-v1

---
+++ Models/clients.py
@@ -0,0 +1,12 @@
+class Client:
+    def __init__(self, id_client: str, nom: str, prenom: str,
+                 mail: str, telephone: str, adresse: str):
+        self.id_client = id_client
+        self.nom = nom
+        self.prenom = prenom
+        self.mail = mail
+        self.telephone = telephone
+        self.adresse = adresse
+
+    def __str__(self):
+        return f"{self.id_client} - {self.prenom} {self.nom} ({self.mail})"
---
+++ Models/vehicules.py
@@ -0,0 +1,14 @@
+class Vehicule:
+    def __init__(self, id_vehicule: str, marque: str, modele: str,
+                 cylindree: int, kilometrage_actuel: float,
+                 date_mise_en_circulation: str):
+        self.id_vehicule = id_vehicule
+        self.marque = marque
+        self.modele = modele
+        self.cylindree = cylindree
+        self.kilometrage_actuel = kilometrage_actuel
+        self.date_mise_en_circulation = date_mise_en_circulation
+
+    def __str__(self):
+        return (f"{self.id_vehicule} - {self.marque} {self.modele} "
+                f"({self.cylindree} cyl., {self.kilometrage_actuel} km)")
